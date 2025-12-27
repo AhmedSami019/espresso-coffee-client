1f608b8e12eb6b031e11f0e7af263c775c30d0f6
ahmedsami019 <easami019192@gmail.com>
2025-12-28 00:37:27 +0600

    added Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import logo from ""
+import logo from "../assets/more/logo1.png"
 
 const Navbar = () => {
     return (
-        <div>
-            <img src="" alt="" />
+        <div className='flex justify-center items-center gap-8 p-5 bg-[#643e38]'>
+            <img className='w-10' src={logo} alt="" />
+            <h2 className='font-bold text-white'>Espresso coffee</h2>
         </div>
     );
 };
--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router";
 import Header from "../Components/Header";
+import Navbar from "../Components/Navbar";
 
 const MainLayout = () => {
   return (
     <div>
-      <Header></Header>
+      <Navbar></Navbar>
       <div className="w-10/12 mx-auto">
         <Outlet></Outlet>
       </div>
