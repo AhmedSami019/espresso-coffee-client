25158c2ecf3c32c75ca3264c16424f8700e49380
ahmedsami019 <easami019192@gmail.com>
2026-01-04 19:53:39 +0600

    user creation compleate

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -3,25 +3,37 @@ import { AuthContext } from "../Context/AuthContext";
 
 const SignUp = () => {
 
-    const {email} =  use(AuthContext)
-    console.log(email);
+    const {createUserWithEmail} =  use(AuthContext)
 
     // create use handler function
+    const handleCreateUser = (e) =>{
+        e.preventDefault()
+        const form = e.target
+        const formData = new FormData(form)
+        const email = formData.get("email")
+        const password = formData.get("password")
+
+        createUserWithEmail(email, password)
+        .then(result => console.log(result))
+        .catch(error => {
+            console.log(error.code);
+        })
+    }
 
   return (
     <div className="card bg-base-100 max-w-md mx-auto shrink-0 shadow-2xl mt-15">
         <h2 className="text-4xl font-extrabold text-center mb-5">Sign Up now</h2>
       <div className="card-body">
-        <fieldset className="fieldset">
+        <form onSubmit={handleCreateUser} className="fieldset">
           <label className="label">Email</label>
-          <input type="email" className="input w-full" placeholder="Email" />
+          <input type="email" name="email" className="input w-full" placeholder="Email" />
           <label className="label">Password</label>
-          <input type="password" className="input w-full" placeholder="Password" />
+          <input type="password" name="password" className="input w-full" placeholder="Password" />
           <div>
             <a className="link link-hover">Forgot password?</a>
           </div>
           <button className="btn btn-neutral mt-4">Login</button>
-        </fieldset>
+        </form>
       </div>
     </div>
   );
